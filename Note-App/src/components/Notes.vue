a64da5b9110b365eb1d119dd468a7baee2dd0ede
Batuhan Tomo <batuhan.tomo@gmail.com>
2023-12-26 17:03:46 +0300

    Refactor Notes.vue component

--- Note-App/src/components/Notes.vue
+++ Note-App/src/components/Notes.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const notes = ref([
   { title: "Not 1", content: "Bu birinci notun içeriğidir." },
@@ -27,50 +27,91 @@ const search = () => {
 
 const createNote = () => {
   createNoteBool.value = true;
-  selectedNote.value = null;
-  newNoteTitle.value = "";
-  newNoteContent.value = "";
+  selectNote(notes.value.length);
+  setTimeout(() => {
+    saveNewNote();
+  }, 0);
 };
 
-const updateNote = () => {
-  const updatedNote = notes[selectedNote.value];
-};
-
-const deleteNote = () => {
-  notes.value.splice(selectedNote.value, 1);
+const deleteNote = (index) => {
+  notes.value.splice(index, 1);
   selectedNote.value = null;
 };
 
 const saveNewNote = () => {
-  if (newNoteTitle.value.trim() !== "" && newNoteContent.value.trim() !== "") {
-    const newNote = {
-      title: newNoteTitle.value,
-      content: newNoteContent.value,
-    };
-    notes.value.push(newNote);
-    selectedNote.value = null;
+  if (newNoteTitle.value.trim() === "") {
+    newNoteTitle.value = "Yeni Not";
   }
+
+  const newNote = {
+    title: newNoteTitle.value.trim(),
+    content: newNoteContent.value.trim(),
+  };
+
+  notes.value.push(newNote);
+
+  console.log(selectedNote.value);
+
+  newNoteTitle.value = "";
+  newNoteContent.value = "";
+  createNoteBool.value = false;
 };
+
+const controlNotes = () => {
+  notes.value.forEach((note, index) => {
+    if (note.title === "Yeni Not" && note.content === "") {
+      deleteNote(index);
+    }
+  });
+};
+
+watch(selectedNote, () => {
+  controlNotes();
+});
 </script>
 
 <template>
-  <div id="app" class="flex p-4 bg-gray-100">
-    <div class="sidebar w-1/3 border-r border-gray-300 p-4">
-      <div class="flex justify-between items-center">
+  <div id="app" class="flex p-4 gap-3 bg-gray-100 bg">
+    <div class="sidebar h-full p-4 divide-x" style="min-width: 300px; overflow-y: auto;">
+      <div class="flex justify-between items-center w-full ">
         <h2
           class="text-2xl font-bold text-gray-800 justify-center items-center"
         >
           Notlar
         </h2>
+        <button
+          class="justify-center items-center plusbutton"
+          @click="createNote()"
+        >
+          <svg
+            class="h-8 w-8 text-yellow-500"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="12" y1="5" x2="12" y2="19" />
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
       </div>
-      <ul>
+      <ul class="w-full">
         <li
           v-for="(note, index) in notes"
           :key="index"
           @click="selectNote(index)"
-          class="cursor-pointer py-2 border-b border-gray-200 hover:bg-gray-200"
+          class="cursor-pointer py-2 flex justify-between items-center"
         >
-          {{ note.title }}
+          {{
+            note.title.length > 20
+              ? note.title.slice(0, 20) + "..."
+              : note.title
+          }}
+          <button @click="deleteNote(index)">
+            <img src="../assets/delete.png" />
+          </button>
         </li>
       </ul>
     </div>
@@ -78,61 +119,18 @@ const saveNewNote = () => {
       <div v-if="selectedNote !== null">
         <div class="topbar flex items-center justify-between mb-4">
           <div>
-            <h2 class="text-2xl font-bold text-gray-800">
-              {{ notes[selectedNote].title }}
-            </h2>
-          </div>
-          <div class="flex">
-            <button @click="createNote" class="new">
-              <img src="../../public/deneme.svg" />
-            </button>
-            <button @click="search" class="search">
-              <img src="../../public/search.svg" />
-            </button>
-            <button @click="deleteNote" class="delete">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
+            <input
+              type="text"
+              v-model="notes[selectedNote].title"
+              class="p-2 rounded mb-4 note-header"
+              style="font-weight: bold; font-size: 24px"
+            />
           </div>
         </div>
         <textarea
           v-model="notes[selectedNote].content"
-          class="w-full h-40 p-2 rounded mb-4 bg-white"
+          class="w-full p-2 rounded mb-4 bg-white max-h-screen"
         ></textarea>
-      </div>
-      <div v-else-if="createNoteBool">
-        <div class="create-note-form">
-          <input
-            v-model="newNoteTitle"
-            type="text"
-            id="newNoteTitle"
-            class="w-full p-2 mb-4 rounded"
-          />
-          <textarea
-            v-model="newNoteContent"
-            id="newNoteContent"
-            class="w-full h-40 p-2 mb-4 first-letter:rounded"
-          ></textarea>
-
-          <button
-            @click="saveNewNote"
-            class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-          >
-            Kaydet
-          </button>
-        </div>
       </div>
       <div v-else>
         <p class="text-gray-500">
@@ -145,7 +143,6 @@ const saveNewNote = () => {
 
 <style>
 body {
-  background-color: #f5f4f4;
   font-family: "Helvetica Neue", Arial, sans-serif;
 }
 
@@ -153,16 +150,31 @@ body {
   border: 1px solid #f5f4f4;
   border-radius: 5px;
   overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+}
+
+.bg {
+  background-image: url("../assets/bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 }
 
 .sidebar {
-  background-color: #d7d6d9;
-  width: 300px;
+  position: sticky;
+  top: 0;
   padding: 20px;
+  background-color: #e8e9e8;
   border-radius: 20px;
 }
 
 .sidebar div {
+  position: sticky;
+  top: -20px;
+  height: 60px;
+  z-index: 1;
+  background-color: #e8e9e8;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -189,85 +201,60 @@ body {
 }
 
 .sidebar li:hover {
-  background-color: #94999d;
+  background-color: #d2d9dd;
 }
 
 .sidebar button {
   display: block;
-  background-color: #4caf50;
   color: #fff;
   border: none;
   border-radius: 4px;
   padding: 10px;
   cursor: pointer;
+  height: 36px;
+  width: 36px;
 }
 
-.sidebar button:hover {
-  background-color: #45a049;
+.sidebar .plusbutton {
+  justify-content: center;
+  align-items: center;
+  padding: 0;
 }
 
-/* content style */
 .content {
-  background-color: #fcf4ed;
   flex: 1;
   padding: 20px;
   border-radius: 20px;
 }
 
-.content .new {
-  background-color: #9d4edd;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  padding: 10px;
-  cursor: pointer;
-  width: 45px;
-  margin-right: 10px;
+.content .note-header {
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  width: 80vw;
+  background-color: transparent;
 }
 
-.content .new:hover {
-  background-color: #7d3caf;
-}
-
-.content .search {
-  background-color: #7fc8f8;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  padding: 10px;
-  cursor: pointer;
-  width: 45px;
-  margin-right: 10px;
-}
-
-.content .search:hover {
-  background-color: #5aa9e6;
-}
-
-.content .delete {
-  background-color: #e5383b;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  padding: 10px;
-  cursor: pointer;
-  width: 45px;
-}
-
-.content .delete:hover {
-  background-color: #ba181b;
+.content .note-header:focus {
+  outline: none;
 }
 
 .create-note-form input {
-  background-color: #fcf4ed;
-  border: lightgray 1px solid;
-  border-radius: 10px;
+  border: none;
+  background-color: transparent;
+}
+
+.create-note-form input:focus {
+  outline: none;
 }
 
 .create-note-form textarea {
-  background-color: #fcf4ed;
-  border: lightgray 1px solid !important;
-  border-radius: 10px !important;
+  border: none !important;
+  background-color: transparent;
+}
+
+.create-note-form textarea:focus {
+  outline: none;
 }
 
 .content h2 {
@@ -283,7 +270,12 @@ body {
   padding: 10px;
   border: none;
   margin-bottom: 20px;
-  background-color: #fcf4ed;
+  background-color: transparent;
+  resize: none;
+  height: 80vh;
+}
+.content textarea:focus {
+  outline: none;
 }
 
 .content button {
